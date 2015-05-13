10aa0de53900acc621a4e8c928b6515603d9c4f8
Nathan Spicer-Davis <boo1098@live.com>
2015-05-12 21:09:01 -0400

    Added more links to social Media

--- includes/footer.php
+++ includes/footer.php
@@ -3,18 +3,23 @@
 		<div id="footer-div">
 			<table width="97%"> 
 				<tr> 
-					<td style="max-width:75%"id="footer-heading"><span class="hvr-pop">More links!</span></td>
+					<td id="footer-heading"><span class="hvr-pop">More links!</span></td>
 					<td class="hvr-grow" align="right" valign="center"><a href="https://twitter.com/FRCTeam3389" class="twitter-follow-button" data-show-count="false">Follow @FRCTeam3389</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></td>
-					<td width="100px"class="hvr-grow" align="right" valign="center"><script src="https://apis.google.com/js/platform.js"></script>
+					<td class="hvr-grow" align="right" valign="center"><script src="https://apis.google.com/js/platform.js"></script>
 						<div class="g-ytsubscribe" data-channel="Robospectrum"></div>
 					</td>
-				</tr>
+					<td class="hvr-grow"><a href="//plus.google.com/u/0/108991739010079990924?prsrc=3" rel="publisher" target="_top" style="text-decoration:none;"><img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/></a></td>				
+					<td class="hvr-grow"><div class="fb-like" data-href="https://www.facebook.com/Team3389" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div></td>
+					
+					</tr>
+					
+					
 			</table>
 			<table style="margin-top:20px;width:98%;table-layout:fixed">
-				<tr style="height:20%">
-					<td align="left" valign="left"><a href="http://usfirst.org"><img align="left" src="resources/first.png" alt="First" height="relative" width="25%" class="hvr-skew-backward"></a></td>
-					<td align="center" valign="center"><a href="http://tecfound.org"><img src="resources/tecfound.png" alt="Tec Foundation" height="relative" width="relative" class="hvr-bounce-in"></a></td>
-					<td align="right" valign="right"><a href="http://usfirst.org/frc"><img align="right"src="resources/frc.gif" alt="FIRST Robotics Competition" height="relative" width="relative" class="hvr-skew-forward"></a></td>
+				<tr style="height:10%">
+					<td  align="left" valign="left"><a href="http://usfirst.org"><img align="left" src="resources/first.png" alt="First" height="30%" width="30%" class="hvr-skew-backward"></a></td>
+					<td align="center" valign="center"><a href="http://tecfound.org"><img src="resources/tecfound.png" alt="Tec Foundation" height="65%" width="relative" class="hvr-bounce-in"></a></td>
+					<td align="right" valign="right"><a href="http://usfirst.org/frc"><img align="right"src="resources/frc.gif" alt="FIRST Robotics Competition" height="65%" width="relative" class="hvr-skew-forward"></a></td>
 				</tr>
 				<tr> 
 					<td style="padding-right:10px" colspan="3" align="right" valign="right"><a href="admin.php" class="hvr-buzz">admins</a></td>
