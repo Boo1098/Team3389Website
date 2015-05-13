6aab5bd48747b4453cc514fa1272e91f905a80cf
Nathan Spicer-Davis <boo1098@live.com>
2015-05-13 18:03:00 -0400

    made them all max-width

--- includes/footer.php
+++ includes/footer.php
@@ -19,15 +19,15 @@ if (isset($_GET["light"])) {
 		
 			<table width="97%"> 
 				<tr style="white-space:no-wrap"> 
-					<td width="50%"id="footer-heading"><span class="hvr-pop">More links!</span></td>
-					<td width="20.5%" class="hvr-grow" align="center" valign="center"><a href="https://twitter.com/FRCTeam3389" class="twitter-follow-button" data-show-count="false">Follow @FRCTeam3389</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></td>
-					<td width="20.5%" class="hvr-grow" align="center" valign="center"><script src="https://apis.google.com/js/platform.js"></script>
+					<td styel="max-width:50%"id="footer-heading"><span class="hvr-pop">More links!</span></td>
+					<td style="max-width:20.5%" class="hvr-grow" align="center" valign="center"><a href="https://twitter.com/FRCTeam3389" class="twitter-follow-button" data-show-count="false">Follow @FRCTeam3389</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></td>
+					<td style="max-width:20.5%" class="hvr-grow" align="center" valign="center"><script src="https://apis.google.com/js/platform.js"></script>
 						<div class="g-ytsubscribe" data-channel="Robospectrum"></div>
 					</td>
-					<td width="20.5%" align="center" class="hvr-grow"><a href="//plus.google.com/u/0/108991739010079990924?prsrc=3" rel="publisher" target="_top" style="text-decoration:none;"><img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/></a></td>				
-					<td width="20.5%" align="center" class="hvr-grow"><div class="fb-like" data-href="https://www.facebook.com/Team3389" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div></td>
+					<td style="max-width:20.5%" align="center" class="hvr-grow"><a href="//plus.google.com/u/0/108991739010079990924?prsrc=3" rel="publisher" target="_top" style="text-decoration:none;"><img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/></a></td>				
+					<td style="max-width:20.5%" align="center" class="hvr-grow"><div class="fb-like" data-href="https://www.facebook.com/Team3389" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div></td>
 					
-					<td width="20.5%" align="right" class="hvr-grow">
+					<td style="max-width:20.5%" align="right" class="hvr-grow">
 						<?php
 						session_start();
 						
