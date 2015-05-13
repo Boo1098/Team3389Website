e9319a562590dc0d819718419974977295d24610
Nathan Spicer-Davis <boo1098@live.com>
2015-05-12 21:08:19 -0400

    Added Contact Page

---
+++ contact.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<?php include("includes/head.php");?>
+	
+	<body>
+	
+		<div class="main-body-text hvr-underline-from-center" id="top">
+			<h2 class="body-header hvr-bounce-in">Contact Us!</h2>
+				<div id="about-us-content" align="center">
+						<?php 
+						
+						include ("markdown.php"); 
+						$Parsedown = new Parsedown();
+						
+						$f = fopen("Markdown/Contact.txt", "r");
+						
+						$markdown = fread($f, 9999999);
+						
+						//$tableHtml = strtok($markdown,'+++');			
+						$markdown = strtok($markdown,'+++');
+						
+						$html = $Parsedown->text($markdown);
+						
+						
+						
+						
+						echo $html;
+						?>
+				</div>
+		</div>
+	</body>
+</div>
+<?php include("includes/footer.php"); ?>
+</html>
