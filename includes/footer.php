b04f61561bb0b610e25192241727a72e1ee98703
Nathan Spicer-Davis <boo1098@live.com>
2015-05-13 16:08:47 -0400

    footer no longer fixed

--- includes/footer.php
+++ includes/footer.php
@@ -17,10 +17,25 @@
 			</table>
 			<table style="margin-top:20px;width:98%;table-layout:fixed">
 				<tr style="height:10%">
-					<td  align="left" style="width:10%" valign="left"><a href="http://usfirst.org"><img align="left" src="resources/first.png" alt="First"height="auto" width="100%"class="hvr-skew-backward"></a></td>
-					<td align="center" style="width:10%" valign="center"><a href="http://tecfound.org"><img src="resources/tecfound.png" alt="Tec Foundation"height="auto" width="100%"class="hvr-bounce-in"></a></td>
-					<td align="right" style="width:10%" valign="right"><a href="http://usfirst.org/frc"><img align="right"src="resources/frc.gif" alt="FIRST Robotics Competition"height="auto" width="100%" class="hvr-skew-forward"></a></td>
+					<td  align="left" style="width:10%" valign="left">
+						<a href="http://usfirst.org">
+							<img align="left" src="resources/first.png" alt="First"height="auto" width="100%"class="hvr-skew-backward">
+						</a>
+					</td>
+					
+					<td align="center" style="width:10%" valign="center">
+						<a href="http://tecfound.org">
+							<img src="resources/tecfound.png" alt="Tec Foundation"height="auto" width="100%"class="hvr-bounce-in">
+						</a>
+					</td>
+					
+					<td align="right" style="width:10%" valign="right">
+						<a href="http://usfirst.org/frc">
+							<img align="right"src="resources/frc.gif" alt="FIRST Robotics Competition"height="auto" width="100%" class="hvr-skew-forward">
+						</a>
+					</td>
 				</tr>
+				
 				<tr> 
 					<td style="padding-right:10px" colspan="3" align="right" valign="right"><a href="admin.php" class="hvr-buzz">admins</a></td>
 				</tr>
