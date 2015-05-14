cc8a02f46634a9eeb460d9a89b228c60c5b33b7e
boo1098 <boo1098@live.com>
2015-05-14 09:05:27 -0400

    fixed typo

--- includes/footer.php
+++ includes/footer.php
@@ -54,7 +54,7 @@
 					</tbody>
 					</table>
 					</div>
-					<p style="padding-top:10px;padding-left:5px;margin:0px;float:left;"align="left"><a style="margin:0px;" href="http://github.com/boo1098/nathan" class="hvr-buzz">Fork me on gihub!</a></p>
+					<p style="padding-top:10px;padding-left:5px;margin:0px;float:left;"align="left"><a style="margin:0px;" href="http://github.com/boo1098/nathan" class="hvr-buzz">Fork me on github!</a></p>
 					<p style="padding-top:10px;padding-right:5px;margin:0px;"align="right"><a style="margin:0px;" href="admin.php" class="hvr-buzz">admins</a></p>
 				
 			</div>
