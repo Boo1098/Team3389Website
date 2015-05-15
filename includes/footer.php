2c66ffcbb30d398b3d3132a12c3bb8ca496d6a3e
Nathan Spicer-Davis <boo1098@live.com>
2015-05-14 20:41:16 -0400

    removed indent

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,7 @@
 					<td id="footer-heading" style="min-width:100px"><span class="hvr-pop">More links!</span></td>
 					<td valign="middle" class="link-row" align="right" width="100%"><a href="https://twitter.com/FRCTeam3389" class="twitter-follow-button"  target="_blank" data-show-count="false">Follow @FRCTeam3389</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 					<script src="https://apis.google.com/js/platform.js"></script>
-						<div class="g-ytsubscribe" data-channel="Robospectrum"></div>
+					<div class="g-ytsubscribe" data-channel="Robospectrum"></div>
 					
 					<a href="//plus.google.com/103918810376285135334/posts?prsrc=3" rel="publisher"  target="_blank" style="text-decoration:none;"><img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/></a>
 					<div class="fb-like" data-href="https://www.facebook.com/Team3389" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
