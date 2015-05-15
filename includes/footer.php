e114f925e01b6657885ca36c84f1c0623901640a
Nathan Spicer-Davis <boo1098@live.com>
2015-05-14 20:16:30 -0400

    added min-width to title

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 		
 			<table width="97%"> 
 				<tr style="white-space:no-wrap;width:98%"> 
-					<td id="footer-heading"><span class="hvr-pop">More links!</span></td>
+					<td id="footer-heading" style="min-width:100px"><span class="hvr-pop">More links!</span></td>
 					<td valign="middle" class="link-row" align="right" width="100%"><a href="https://twitter.com/FRCTeam3389" class="twitter-follow-button"  target="_blank" data-show-count="false">Follow @FRCTeam3389</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 					<script src="https://apis.google.com/js/platform.js"></script>
 						<div class="g-ytsubscribe" data-channel="Robospectrum"></div>
