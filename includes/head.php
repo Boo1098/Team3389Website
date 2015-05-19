1d9ceb35f098383e46ae252e6418487cba17c391
Nathan Spicer-Davis <boo1098@live.com>
2015-05-18 21:33:58 -0400

    3d float effect

--- includes/head.php
+++ includes/head.php
@@ -30,7 +30,7 @@ if (isset($_GET["light"])) {
 }(document, 'script', 'facebook-jssdk'));</script>
 	
 	<title>Tec Tigers: Team 3389</title>
-	<link href='http://fonts.googleapis.com/css?family=Roboto&effect=anaglyph' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto&effect=3d-float' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="Stylesheets/stylesheet.css">
 	<link rel="stylesheet" type="text/css" href="Stylesheets/hover.css">
 	<link rel="javascript" type="javascript" href="https://apis.google.com/js/platform.js">
@@ -52,7 +52,7 @@ if (isset($_GET["light"])) {
 	<header>
 		<table style="width:100%" id="header-table">
 			<tr>
-				<td width="172px" id="head-text"><a href="http://nathan.team3389.info" class="font-effect-anaglyph"> &nbsp;Tec Tigers</a></td>
+				<td width="172px" id="head-text"><a href="http://nathan.team3389.info" class="font-effect-3d-float"> &nbsp;Tec Tigers</a></td>
 				<td width="77px" id="logo"><img src="resources/logo-small.png" alt="logo" height="50px" width="75px" class="hvr-grow-rotate"></td>
 				<td id="header-spacer"></td>
 				<td class="header-links" align="right" style="padding-right:10px"><a  class="header-links hvr-grow" href="contact.php">Contact Us</a> | <a  class="header-links hvr-grow" href="Sponsors.php">Sponsors</a> | <a  class="header-links hvr-grow" href="history.php">History</a></td></tr>
