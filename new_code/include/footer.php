5bf02f4e42fa917d334135cc815e3716bc7c1eaa
boo1098 <boo1098@live.com>
2015-09-22 22:03:12 -0400

    closed some tags!

---
+++ new_code/include/footer.php
@@ -0,0 +1,79 @@
+<footer>
+	<div id="footer-div">
+	
+		<table width="97%"> 
+			<tr style="white-space:no-wrap;width:98%"> 
+				
+				<!-- Twitter-->
+				<td id="footer-heading" style="min-width:100px"><span class="hvr-pop">More links!</span></td>
+				<style>
+				.twitter-follow-button {
+					padding-top:8px !important;
+				}
+				</style>
+				<td valign="middle" class="link-row" align="right" width="100%"><a href="https://twitter.com/FRCTeam3389" class="twitter-follow-button"  target="_blank" data-show-count="false" style="padding-top:8px !important;">Follow @FRCTeam3389</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+					
+				<!-- Youtube -->
+				<script src="https://apis.google.com/js/platform.js"></script>
+				<div class="g-ytsubscribe" data-channel="Robospectrum"></div>
+				
+				<!--- Google+ -->
+				<a href="//plus.google.com/103918810376285135334/posts?prsrc=3" rel="publisher"  target="_blank" style="text-decoration:none; padding-top:10px; padding-left:20px;"><img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/></a>
+				<div class="fb-like" data-href="https://www.facebook.com/Team3389" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true" style="padding-top:5px;"></div>
+					
+				
+					<?php
+						/*
+						session_start();
+						
+						if (isset($_SESSION["theme"])) {
+							if ($_SESSION["theme"] == "light") {
+								echo "<a style=\"padding-top:5px\" href=\"?dark=1\">Dark theme\n";							
+							}
+							if ($_SESSION["theme"] == "dark") {
+								echo "<a style=\"padding-top:5px\" href=\"?light=1\">Light theme\n";							
+							}
+						} */
+					?>
+					
+				</td>
+			</tr>
+		</table>
+			
+			<div style="background-color:#3A3A3A;align:center;">
+				<div style="width:100%;height:100px">
+					<table width="98%">
+						<tbody>
+						<tr>
+							<td>
+								<a href="http://usfirst.org">
+									<img align="left" src="resources/first.png" style="align:left" alt="First"height="100px" target="_blank" width="auto" class="hvr-skew-backward">
+								</a>
+							</td>
+							
+							<td valign="center">
+								<a style="display:block; margin-left:auto; margin-right:auto" target="_blank" href="http://tecfound.org">
+									<img src="resources/tecfound-big.png" style="display:block; margin-left:auto; margin-right:auto" alt="Tec Foundation"height="100px" width="auto" class="hvr-bounce-in">
+								</a>
+							</td>
+							
+							<td>
+								<a href="http://www.usfirst.org/roboticsprograms/frc" target="_blank" >
+									<img align="right" style="align:right" src="resources/frc.gif" alt="FIRST Robotics Competition"height="100px" width="auto" class="hvr-skew-forward">
+								</a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		
+		
+			<p style="padding-top:10px;padding-left:5px;margin:0px;float:left;"align="left"><a style="margin:0px;" href="http://github.com/boo1098/nathan" target="_blank" class="hvr-buzz">Fork me on github!</a></p>
+		
+			<p style="padding-top:10px;padding-right:5px;margin:0px;"align="right"><a style="margin:0px;" href="admin.php" class="hvr-buzz">admins</a></p>
+				
+		</div>
+	</div>
+</footer>
+</body>
+</html>
---
+++ new_code/include/head.php
@@ -0,0 +1,74 @@
+<DOCTYPE! html>
+
+
+	<head>
+		<meta charset="UTF-8">
+		<?php/*
+			session_start();
+
+			if (!isset($_SESSION["theme"])) {
+				$_SESSION["theme"] = "light";
+			}
+			if (isset($_GET["dark"])) {
+				if ($_GET["dark"] == 1) {
+					$_SESSION["theme"] = "dark";		
+				}			
+			}
+			if (isset($_GET["light"])) {
+				if ($_GET["light"] = 1) {
+					$_SESSION["theme"] = "light";		
+				}
+			}*/
+
+		?>
+
+		<div id="fb-root"></div>
+
+		<script>(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+	
+		<title>Tec Tigers: Team 3389</title>
+		
+		<link href='http://fonts.googleapis.com/css?family=Roboto&effect=3d-float' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="Stylesheets/stylesheet.css">
+		<link rel="stylesheet" type="text/css" href="Stylesheets/hover.css">
+		<link rel="javascript" type="javascript" href="https://apis.google.com/js/platform.js">
+
+		<!---<style>
+		
+		<?php
+			/*session_start();
+			if (isset($_SESSION["theme"])) {
+				if ($_SESSION["theme"] == "dark") {
+					include('Stylesheets/dark.css');	
+			}
+				if ($_SESSION["theme"] == "light") {
+					include('Stylesheets/light.css');
+				}
+			}
+			*/
+		?>
+		
+		</style>-->
+		
+	</head>
+	
+	<body>
+		<div id=wrapper style="margin-bottom:5px;min-height:82%;">
+		<header>
+		
+			<table style="width:100%" id="header-table">
+				<tr>
+					<td width="172px" id="head-text"><a href="http://nathan.team3389.info" class="font-effect-3d-float"> &nbsp;TEC Tigers</a></td>
+					<td width="77px" id="logo"><img src="resources/logo2-small.png" alt="logo" height="50px" width="50px" class="hvr-grow-rotate"></td>
+					<td id="header-spacer"></td>
+					<td class="header-links" align="right" style="padding-right:10px"><a  class="header-links hvr-grow" href="contact.php">Contact Us</a> | <a  class="header-links hvr-grow" href="Sponsors.php">Sponsors</a> | <a  class="header-links hvr-grow" href="history.php">History</a> | <a  class="header-links hvr-grow" href="mentors.php">Mentors</a></td></tr>
+			</table>
+	
+		</header>
+	
