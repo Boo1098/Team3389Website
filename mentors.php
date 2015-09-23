176741500d3af1a4fdefe711765812d0f39d6816
boo1098 <boo1098@live.com>
2015-09-23 19:00:53 -0400

    removed all markdown from mentors page

--- mentors.php
+++ mentors.php
@@ -1,27 +1,11 @@
 <!DOCTYPE html>
 <?php include("includes/head.php");?>
+	<div class="main-body-text hvr-underline-from-center" id="top">
+		<h2 class="body-header hvr-bounce-in font-effect-3d-float">Mentors</h2>
 	
-
-						<?php 
-
-						include ("markdown.php"); 
-						$Parsedown = new Parsedown();
-						
-						$f = fopen("Markdown/mentors.txt", "r");
-						
-						$markdown = fread($f, 9999999);
-						
-						//$tableHtml = strtok($markdown,'+++');			
-						
-						
-						$html = $Parsedown->text($markdown);
-						
-						
-						
-						
-						echo $html;
-						?>
-
-
+		<div id="about-us-content" align="center">
+			<p>He cool</p>
+		</div>
+	</div>
 <?php include("includes/footer.php"); ?>
 </html>
