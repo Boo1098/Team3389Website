2a7c74ce1cff83d2b74e0a775c707828aca7f3ab
boo1098 <boo1098@live.com>
2015-09-23 14:25:08 -0400

    fixed formatting for sponsors page

--- Sponsors.php
+++ Sponsors.php
@@ -1,33 +1,27 @@
 <!DOCTYPE html>
 <?php include("includes/head.php");?>
-	
-	<body>
-	
-		<div class="main-body-text hvr-underline-from-center" id="top">
-			<h2 class="body-header hvr-bounce-in font-effect-3d-float">2015 Sponsors</h2>
-				<div id="about-us-content" align="center">
-						<?php 
+	<div class="main-body-text hvr-underline-from-center" id="top">
+		<h2 class="body-header hvr-bounce-in font-effect-3d-float">2015 Sponsors</h2>
+		<div id="about-us-content" align="center">
+			<?php 
+					include ("markdown.php"); 
+					$Parsedown = new Parsedown();
 						
-						include ("markdown.php"); 
-						$Parsedown = new Parsedown();
+					$f = fopen("Markdown/Sponsors.txt", "r");
 						
-						$f = fopen("Markdown/Sponsors.txt", "r");
+					$markdown = fread($f, 9999999);
 						
-						$markdown = fread($f, 9999999);
+					//$tableHtml = strtok($markdown,'+++');			
+					$markdown = strtok($markdown,'+++');
 						
-						//$tableHtml = strtok($markdown,'+++');			
-						$markdown = strtok($markdown,'+++');
-						
-						$html = $Parsedown->text($markdown);
+					$html = $Parsedown->text($markdown);
 						
 						
 						
 						
-						echo $html;
-						?>
-				</div>
+					echo $html;
+			?>
 		</div>
-	</body>
-</div>
+	</div>
 <?php include("includes/footer.php"); ?>
 </html>
