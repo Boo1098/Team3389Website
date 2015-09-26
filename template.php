b3407e55e731236b5870477bea11c73aa330524e
Nathan Spicer-Davis <boo1098@live.com>
2015-09-26 11:39:09 -0400

    remformatted template

--- template.php
+++ template.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <?php include("includes/head.php"); ?>
-	
 	<body>
-	
 		<div class="main-body-text" id="top">
-			<h2 class="body-header">About us!</h2>
+			<h2 class="body-header">About FTC</h2>
 			
 			<table id="about-us-content">
 				<tr>
-					<td><img src="resources/logo.png" alt="us" height="200px" width="relative" class="hvr-grow">
+					<td>
+						<img src="resources/logo.png" alt="us" height="200px" width="relative">
 					</td>
 					<td>
 						<p class="body-text">We are <a class="body-link" href="#">TEST</a> (For Inspiration and Recognition of Science and Technology) robotics team number 3389 and our team name is TEC Tigers. We are located at Parkside High School - CTE, Wicomico County, Salisbury MD 21804.</p>
