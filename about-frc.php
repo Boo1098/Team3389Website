a506e8d6c724bfba0d54de1249d4fa399194dc24
Nathan Spicer-Davis <boo1098@live.com>
2015-09-27 12:27:30 -0400

    added content to about-frc

--- about-frc.php
+++ about-frc.php
@@ -10,7 +10,9 @@
 						<img src="resources/logo.png" alt="us" height="200px" width="relative">
 					</td>
 					<td>
-						<p class="body-text">FRC is the First robotic competition. Each year, teams from accross the country build a robot to complete a specific task and then take to competitions. Our team has been doing FRC for the past 7??? years. Last years challenge was called <a class="body-link" href="https://www.youtube.com/watch?v=W6UYFKNGHJ8">Recycle Rush</a>. The challenge was to make a robot that can pick up crates and move them into a storing area. Other parts include putting recycling bins on top of the stacks of crates as a multiplier and putting "litter" into the recycling bins for poings.<a class="body-link" href="#">TEST</a></p>
+						<p class="body-text">FRC is the First robotic competition. Each year, teams from across the country build a robot to complete a specific task and then take to competitions. Our team has been doing FRC for the past 7??? years. Last years challenge was called <a class="body-link" href="https://www.youtube.com/watch?v=W6UYFKNGHJ8">Recycle Rush</a>. The challenge was to make a robot that can pick up crates and move them into a storing area. Other parts include putting recycling bins on top of the stacks of crates as a multiplier and putting "litter" into the recycling bins for points. Our robot last year, name "This side UP", was made pneumatics lifted up the crates and stacked them simultaneously.</p>
+						
+						<p>This Program involves mechanical engineering programming and CAD as well. Students can pick to specialize in specific categories like CAD team or mechanical team. The build season to build the robot is actually only 6 weeks, making meetings intense and sometimes longer. During build season we usally have meetings Monday-Friday, and sometimes (if we're behind schedule) on Saturday.</p>
 					</td>
 				</tr>
 			</table>
