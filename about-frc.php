b1808cc9cfc15f646d69dec78adc1fa88b12b20b
Nathan Spicer-Davis <boo1098@live.com>
2015-09-27 19:25:57 -0400

    float

--- about-frc.php
+++ about-frc.php
@@ -6,7 +6,7 @@
 			
 			<br><br>
 			<div id="about-us-content">
-				<img src="resources/first-logo-reversed.png" alt="us" height="200px" width="relative">
+				<img style="float:left;" src="resources/first-logo-reversed.png" alt="frc" height="200px" width="relative">
 				<p class="body-text">FRC is the First robotic competition. Each year, teams from across the country build a robot to complete a specific task and then take to competitions. Our team has been doing FRC for the past 7??? years. Last years challenge was called <a class="body-link" href="https://www.youtube.com/watch?v=W6UYFKNGHJ8">Recycle Rush</a>. The challenge was to make a robot that can pick up crates and move them into a storing area. Other parts include putting recycling bins on top of the stacks of crates as a multiplier and putting "litter" into the recycling bins for points. Our robot last year, name "This side UP", was made pneumatics lifted up the crates and stacked them simultaneously.</p>
 						
 				<p>This Program involves mechanical engineering programming and CAD as well. Students can pick to specialize in specific categories like CAD team or mechanical team. The build season to build the robot is actually only 6 weeks, making meetings intense and sometimes longer. During build season we usally have meetings Monday-Friday, and sometimes (if we're behind schedule) on Saturday.</p>
