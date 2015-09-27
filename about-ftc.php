eac2f6546567265b909e1597a1f3b726ff5a71f7
Nathan Spicer-Davis <boo1098@live.com>
2015-09-27 19:43:27 -0400

    Added about-ftc page

---
+++ about-ftc.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<?php include("includes/head.php");?>
+	
+		<div class="main-body-text" id="top">
+			<h2 class="body-header font-effect-3d-float">About us!</h2>
+			
+			<br />
+			<div id="about-us-content">
+				<img style="float:left;" src="resources/FTC-logo-reversed.png" alt="frc" height="200px" width="relative">
+				<p class="body-text"><a class="body-link" href="https://youtu.be/b49Q3kqrSv0">FIRST Rescue</a></p>
+		</div>
+	</body>
+</div>
+<?php include("includes/footer.php"); ?>
+</html>
