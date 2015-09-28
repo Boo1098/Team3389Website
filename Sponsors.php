800b62143f2435cd1ff327d5b4986522fd941f77
Nathan Spicer-Davis <boo1098@live.com>
2015-09-27 20:45:28 -0400

    made sponsors page fit sponsors

--- Sponsors.php
+++ Sponsors.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <?php include("includes/head.php");?>
-	<div class="main-body-text" id="top">
+	<div class="main-body-text" height="400px" id="top">
 		<h2 class="body-header font-effect-3d-float">2015 Sponsors</h2>
-		<div id="about-us-content" align="center">
+		<div id="about-us-content" height="200px "align="center">
 			<img src="resources/sponsors/nasa.png" alt="nasa" height="200px">
 			<img src="resources/sponsors/ace.png" alt="ace" height="200px">
 			<img src="resources/sponsors/community-foundation.png" alt="Community Foundation" height="200px">
