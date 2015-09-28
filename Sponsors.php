c381d111fab92b2f3b63a80a29c4039930e4282e
Nathan Spicer-Davis <boo1098@live.com>
2015-09-27 20:53:18 -0400

    gave sponsor images padding

--- Sponsors.php
+++ Sponsors.php
@@ -3,11 +3,11 @@
 	<div class="main-body-text" height="400px" id="top">
 		<h2 class="body-header font-effect-3d-float">2015 Sponsors</h2>
 		<div id="about-us-content" height="200px "align="center">
-			<img src="resources/sponsors/nasa.png" alt="nasa" height="200px">
-			<img src="resources/sponsors/ace.png" alt="ace" height="200px">
-			<img src="resources/sponsors/community-foundation.png" alt="Community Foundation" height="200px">
-			<img src="resources/sponsors/Parkside-CTE.png" alt="Parkside CTE" height="200px">
-			<img src="resources/sponsors/techub.png" alt="TEC hub" height="200px">
+			<img src="resources/sponsors/nasa.png" class="sponsor-images" alt="nasa" height="200px">
+			<img src="resources/sponsors/ace.png" class="sponsor-images" alt="ace" height="200px">
+			<img src="resources/sponsors/community-foundation.png" class="sponsor-images" alt="Community Foundation" height="200px">
+			<img src="resources/sponsors/Parkside-CTE.png" class="sponsor-images" alt="Parkside CTE" height="200px">
+			<img src="resources/sponsors/techub.png" class="sponsor-images" alt="TEC hub" height="200px">
 		</div>
 	</div>
 <?php include("includes/footer.php"); ?>
