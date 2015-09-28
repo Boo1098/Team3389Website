baebf73bca49de45fcc76a5a1075c428dac68ee0
Nathan Spicer-Davis <boo1098@live.com>
2015-09-27 20:15:36 -0400

    changed logo on about-ftc page

--- about-ftc.php
+++ about-ftc.php
@@ -6,7 +6,7 @@
 			
 			<br />
 			<div id="about-us-content">
-				<img style="float:left;" src="resources/FTC-logo.png" alt="frc" height="150px" width="relative">
+				<img style="float:left;" src="resources/FTC-logo-withshapes-reversed.png" alt="frc" height="150px" width="relative">
 				<p class="body-text">FTC if almost like <a class="body-link" href="about-frc.php">FRC</a>, but has very large differences. The <em>FIRST</em> Tech Challenge is about building a very small robot that can accomplish a lot. This years challenge is <a class="body-link" href="https://youtu.be/b49Q3kqrSv0">FIRST Rescue</a> and this is our teams first(get it?) year doing the FTC program. All of our students are looking forward to building and competing in this years compatition.</p>
 				
 				<p>The object of this years FTC game is being able to climb a mountain and also collecting and moving debris. All while keeping your robot in an 18x18x18in cube square. After the match starts though your robot is allowed to stretch to any size. There is also a 30 second autonomous period at the begining of each match. This is where programming comes in. If a robot can score big points during the autonomous period it will do very well in the competition.</p>
