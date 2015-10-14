a0b38e90aa6250819eade5ed778d7ec427103841
Nathan Spicer-Davis <boo1098@live.com>
2015-10-14 19:54:17 -0400

    added slideshows dir

---
+++ slideshows/template.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php include("includes/head.php");?>
+	
+		<div class="main-body-text" id="top">
+			<h2 class="body-header font-effect-3d-float">Pictures</h2>
+			
+			<br />
+			<div id="about-us-content">
+				<img style="float:left;" src="resources/FRC-logo-reversed.png" alt="frc" height="200px" width="relative">
+				<p class="body-text">FRC is the FIRST robotics competition. Each year, teams from across the country build a robot to complete a specific task and then take to competitions. Our team has been doing FRC for the past 7??? years. Last years challenge was called <a class="body-link" href="https://www.youtube.com/watch?v=W6UYFKNGHJ8">Recycle Rush</a>. The challenge was to make a robot that can pick up crates and move them into a storing area. Other parts include putting recycling bins on top of the stacks of crates as a multiplier and putting "litter" into the recycling bins for points. Our robot last year, name "This side UP", was made pneumatics lifted up the crates and stacked them simultaneously.</p>
+			</div>
+		</div>
+	</body>
+</div>
+<?php include("includes/footer.php"); ?>
+</html>
