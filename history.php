9798e1c1998d970f6809286e8666a3a5befa0c41
boo1098 <boo1098@live.com>
2015-10-15 10:22:46 -0400

    fixed formatting on history

--- history.php
+++ history.php
@@ -134,12 +134,13 @@ include("includes/head.php");
 				<p>Placed 36th out of 60 teams present</p>
 				
 				<!--- Thanks for writing this! --->
-				<p align="left">During the second year the students tried their best to persevere even with only one year experience from the "elites" of the group. With a tough set of rules and constraints the team built a functional robot with little to no initial problems, however after arriving at the competition problems began to arise in the form of that magic smoke you see only three times in life:
-					<ol>
+				<p align="left">During the second year the students tried their best to persevere even with only one year experience from the "elites" of the group. With a tough set of rules and constraints the team built a functional robot with little to no initial problems, however after arriving at the competition problems began to arise in the form of that magic smoke you see only three times in life:</p>
+					<ol text-align="right">
 						<li>When you get Mr. Miles angry</li>
 						<li>When you burn out a motor</li>
 						<li>When Mrs. Miles sees a you drinking in the lab</li>
 					</ol>
+				<p align="left">
 				Even with these issues  we managed to come in 36<sup>th</sup> out of 60 teams competing.
 				</p>
 
