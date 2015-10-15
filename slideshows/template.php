b5f650bee43922c6c646ac3775cba8421386fd57
Nathan Spicer-Davis <boo1098@live.com>
2015-10-14 20:01:40 -0400

    added html for slideshow to template

--- slideshows/template.php
+++ slideshows/template.php
@@ -9,8 +9,17 @@ include("includes/head.php");
 			
 			<br />
 			<div id="about-us-content">
-				<img style="float:left;" src="resources/FRC-logo-reversed.png" alt="frc" height="200px" width="relative">
-				<p class="body-text">FRC is the FIRST robotics competition. Each year, teams from across the country build a robot to complete a specific task and then take to competitions. Our team has been doing FRC for the past 7??? years. Last years challenge was called <a class="body-link" href="https://www.youtube.com/watch?v=W6UYFKNGHJ8">Recycle Rush</a>. The challenge was to make a robot that can pick up crates and move them into a storing area. Other parts include putting recycling bins on top of the stacks of crates as a multiplier and putting "litter" into the recycling bins for points. Our robot last year, name "This side UP", was made pneumatics lifted up the crates and stacked them simultaneously.</p>
+				
+				<div id="jslide">
+					<img src="http://placehold.it/350&text=01">
+					<img src="http://placehold.it/350&text=02">
+					<img src="http://placehold.it/350&text=03">
+					<img src="http://placehold.it/350&text=04">
+					<img src="http://placehold.it/350&text=05">
+				</div>
+
+				<!-- SCRIPTS -->
+				<script src="jslide.js"></script>
 			</div>
 		</div>
 	</body>
