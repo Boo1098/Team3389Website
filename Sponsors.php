c423b4bf843fd4ef12178de030ffaef7d6eebe45
Nathan Spicer-Davis <boo1098@live.com>
2016-01-21 19:58:57 -0500

    MADER

--- Sponsors.php
+++ Sponsors.php
@@ -22,7 +22,7 @@ include("includes/head.php");
 			<img src="http://team3389.info/resources/sponsors/new/d3corp.jpg" class="sponsor-images" alt="D3 Corp" height="200px" />
 			<img src="http://team3389.info/resources/sponsors/new/futuristic-logo-onDark-100high.png" class="sponsor-images" alt="Futuristic" height="200px" />
 			<img src="http://team3389.info/resources/sponsors/new/joyLogoLiquid.jpg" class="sponsor-images" alt="Joy" height="200px" />
-			<img src="http://team3389.info/resources/sponsors/new/mader.gif" class="sponsor-images" alt="Mader Systems Consulting" height="200px" />
+			<img src="http://team3389.info/resources/sponsors/new/mader.gif" class="sponsor-images" alt="Mader Systems Consulting" height="50px" />
 		</div>
 	</div>
 <?php include("includes/footer.php"); ?>
