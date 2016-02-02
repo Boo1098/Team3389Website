e9f6f7ae1a9e3be9e1be30332692f6f0cf7db8e7
Nathan Spicer-Davis <Boo1098@users.noreply.github.com>
2016-02-02 16:26:38 -0500

    program select include

---
+++ includes/programselect.php
@@ -0,0 +1,25 @@
+				<div id="program_select">
+					<div id="vex_select">
+						<h2><img src="http://www.team3389.info/resources/VEX-icon-white.png" alt="VEX" /></h2>
+						<div class="program-text">
+							<p>Easy to manage small robot with a select group of parts making everything easy to put together.</p>
+						</div>
+						<p class="more_info_button"><a href="about-vex.php">Learn More</a></p>
+					</div><!--
+					
+				--><div id="ftc_select">
+						<h2><img src="http://team3389.info/resources/FIRST/Programs/Typography/White/FIRSTTech_Type_White.png" alt="FIRST Tech challenge" /></h2>
+						<div class="program-text">
+							<p>The newest addition to team3389, FIRST<sup>&reg;</sup> Tech challenge focuses on the tech side requiring a smaller robot to do a lot more tasks.</p>
+						</div>
+						<p class="more_info_button"><a href="about-ftc.php">Learn More</a></p>
+					</div><!--
+					
+				--><div id="frc_select">
+						<h2><img src="http://team3389.info/resources/FIRST/Programs/Typography/White/FIRSTRobotics_Type_White.png" alt="FIRST Robotics Competition" /></h2>
+						<div class="program-text">
+							<p>Big robot that needs lots of teamwork and dedication with only a 6 week period to work on it.</p>
+						</div>
+						<p class="more_info_button"><a href="about-frc.php">Learn More</a></p>
+					</div>
+				</div>
