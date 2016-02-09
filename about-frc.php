afb93cccf31c69cabb28cdaecce23393bfb6889b
unknown <nathansd1098@gmail.com>
2016-02-09 17:26:25 -0500

    Editiing

--- about-frc.php
+++ about-frc.php
@@ -11,7 +11,7 @@ include("includes/head.php");
 			<div id="about-us-content">
 				<img style="float:left;padding:20px;" src="http://team3389.info/resources/FIRST/Programs/IconVertical/RGB/Reverse/FIRSTRobotics_IconVert_RGB_reverse.png" class="full_mobile" alt="frc" height="200" />
 				
-				<!-- THANKS for writing this ALEX -->
+				<!-- THANKS for writing this ALEX  -->
 				
 				<div class="text">
 					<p><em>FIRST</em><sup>&reg;</sup> Robotics Competition (FRC) is an international high school robotics competition. Each year, teams of high school students and mentors work during a six-week period to build game-playing robots that weigh up to 120 pounds (54 kg). Robots complete tasks such as scoring balls into goals, Frisbees into goals, inner tubes onto racks, hanging on bars, and balancing robots on balance beams, for example this year's "challenge" was <a class="body-link" href="https://www.youtube.com/watch?v=W6UYFKNGHJ8">Recycle Rush</a>. The game changes yearly, keeping the excitement fresh and giving each team a more level playing field. While teams are given a standard set of parts, they are also allowed a budget and encouraged to buy or make specialized parts. FRC is one of four robotics competition programs organized by <em>FIRST</em>.</p>
--- history10.php
+++ history10.php
@@ -21,14 +21,7 @@ include("includes/head.php");
 					<p class="center">Placed 36th out of 60 teams present</p>
 					
 					<!-- Thanks for writing this Alex! -->
-					<p>During the second year the students tried their best to persevere even with only one year experience from the "elites" of the group. With a tough set of rules and constraints the team built a functional robot with little to no initial problems, however after arriving at the competition problems began to arise in the form of that magic smoke you see only three times in life:</p>
-						<ol>
-							<li>When you get Mr. Miles angry</li>
-							<li>When you burn out a motor</li>
-							<li>When Mrs. Miles sees a you drinking in the lab</li>
-						</ol>
-					<p>
-					Even with these issues  we managed to come in 36<sup>th</sup> out of 60 teams competing.
+					<p>During the second year the students tried their best to persevere even with only one year experience from the veterans of the group. With a tough set of rules and constraints the team built a functional robot with little to no initial problems. However, after arriving at the competition problems began to arise. Even with these issues  we managed to come in 36<sup>th</sup> out of 60 teams competing.
 					</p>
 					
 					<a style="float:left" href="history9.php">&lt;-Previous Year</a><a style="float:right" href="history11.php">Next Year-&gt;</a><p>&nbsp;</p>
