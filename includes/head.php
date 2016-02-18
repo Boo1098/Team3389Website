6bc4b383d2787b67a2e4f045a73a2ba110c8c90b
Nathan Spicer-Davis <boo1098@live.com>
2016-02-17 21:57:36 -0500

    changed analytics

--- includes/head.php
+++ includes/head.php
@@ -48,13 +48,7 @@ if (isset($_GET["light"])) {
 	<body>
 	<?php include('/includes/analytics.php'); ?>
 	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
+	<script src="/includes/anayltics.js"></script>
 	<!--<script src="/includes/jquery-2.1.4.min.js"></script>
 	<script src="/includes/doubletaptogo.min.js"></script>-->
 <div id=wrapper style="margin-bottom:5px;">
