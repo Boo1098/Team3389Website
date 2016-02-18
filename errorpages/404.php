1c061e988c776d8185489d119074c734c96bbe35
Nathan Spicer-Davis <boo1098@live.com>
2016-02-17 22:49:06 -0500

    absolute links

--- errorpages/404.php
+++ errorpages/404.php
@@ -6,7 +6,7 @@
 	</head>
 	
 	<body>
-		<img src="tec_logo-compressed.jpg" alt="TEC Tigers" height="100" /><br /><br />
+		<img src="http://www.team3389.info/errorpages/tec_logo-compressed.jpg" alt="TEC Tigers" height="100" /><br /><br />
 		<h1 style="font-family:'Pinyon Script', cursive, sans-serif;font-size: 5em;display:inline"><span style="font-family: Calibri, sans-serif;">Error:</span> 404</h1>
 		<h2 style="display:inline;padding-left:20px;">We must have changed something :(</h2>
 		<p style="font-size:1em;">We have been making lots of changes and you may have been linked to an old page. If you feel there should be a page please email the admin at na<span style="display:none">ajksldf</span>thansd1098@gm<span style="display:none">assdklfj.com</span>ail.com</p>
