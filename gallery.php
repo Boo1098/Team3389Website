859a8fc0cdf9f3dfb871686319a35aad4ff5beee
Nathan Spicer-Davis <boo1098@live.com>
2016-02-22 21:57:46 -0500

    stuff

--- gallery.php
+++ gallery.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php 
-$title = "Sponsors";
+$title = "Gallery";
 include("includes/head.php");
 ?>
 	<style>
@@ -12,11 +12,20 @@ include("includes/head.php");
 		<h2 class="body-header font-effect-3d-float">Gallery</h2>
 		<div id="about-us-content" height="200 "align="center">
 			<?php
+			
+				if($_GET['file'] !== ""){
 				$files = glob("../../team3389.info/old_site/img2/galleries/2010-Chesapeake_Regional/tn/*.*");
 				for ($i=0; $i<count($files); $i++)
 				{
 					$num = $files[$i];
-					echo '<a href="'.'http://www.team3389.info/old_site/img2/galleries/2010-Chesapeake_Regional/'.substr($num,75).'"><img src="'.'http://'.substr($num,6).'" alt="'.substr($num,75,-4).'" style="float:left;" /></a>';				}
+					echo '<a href="'.'http://www.team3389.info/old_site/img2/galleries/2010-Chesapeake_Regional/'.substr($num,75).'"><img src="'.'http://'.substr($num,6).'" alt="'.substr($num,75,-4).'" style="float:left;" /></a>';				
+				}} else {
+					$files = glob("../../team3389.info/old_site/img2/galleries/*")
+					for($i =0; $i<count($files);$i++){
+						$num = $files[$i];
+						echo $num;
+					}
+				}
 			?>
 			
 			<div style="clear:both;"></div>
