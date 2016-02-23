14c695f345614f8bbe7de5f0b931d3ae84243c9b
Nathan Spicer-Davis <boo1098@live.com>
2016-02-22 21:26:54 -0500

    test gallery page

---
+++ gallery.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php 
+$title = "Sponsors";
+include("includes/head.php");
+?>
+	<div class="main-body-text" height="400" id="top">
+		<h2 class="body-header font-effect-3d-float">Gallery</h2>
+		<div id="about-us-content" height="200 "align="center">
+			<?php
+				$files = glob("http://www.team3389.info/old_site/img2/galleries/2010-Chesapeake_Regional/tn/*.*");
+				for ($i=0; $i<count($files); $i++)
+				{
+					$num = $files[$i];
+					echo '<a href="'.'http://www.team3389.info/old_site/img2/galleries/2010-Chesapeake_Regional/'.substr($num,77).'"><img src="'.$num.'" alt="'.substr($num,77,-4).'" /></a>'."<br><br>";
+				}
+			?>
+		</div>
+	</div>
+<?php include("includes/footer.php"); ?>
+</html>
