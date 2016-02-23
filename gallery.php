13a90a175033d285101ef3f36dfa8468d701b85c
Nathan Spicer-Davis <boo1098@live.com>
2016-02-22 21:59:13 -0500

    semi colons

--- gallery.php
+++ gallery.php
@@ -15,12 +15,13 @@ include("includes/head.php");
 			
 				if($_GET['file'] !== ""){
 				$files = glob("../../team3389.info/old_site/img2/galleries/2010-Chesapeake_Regional/tn/*.*");
-				for ($i=0; $i<count($files); $i++)
-				{
-					$num = $files[$i];
-					echo '<a href="'.'http://www.team3389.info/old_site/img2/galleries/2010-Chesapeake_Regional/'.substr($num,75).'"><img src="'.'http://'.substr($num,6).'" alt="'.substr($num,75,-4).'" style="float:left;" /></a>';				
-				}} else {
-					$files = glob("../../team3389.info/old_site/img2/galleries/*")
+					for ($i=0; $i<count($files); $i++)
+					{
+						$num = $files[$i];
+						echo '<a href="'.'http://www.team3389.info/old_site/img2/galleries/2010-Chesapeake_Regional/'.substr($num,75).'"><img src="'.'http://'.substr($num,6).'" alt="'.substr($num,75,-4).'" style="float:left;" /></a>';				
+					}
+				} else {
+					$files = glob("../../team3389.info/old_site/img2/galleries/*");
 					for($i =0; $i<count($files);$i++){
 						$num = $files[$i];
 						echo $num;
