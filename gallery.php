ec877988ca5248cc37c287cac5f89c437eebe2fe
Nathan Spicer-Davis <boo1098@live.com>
2016-02-24 18:20:06 -0500

    added padding and floats

--- gallery.php
+++ gallery.php
@@ -25,7 +25,7 @@ include("includes/head.php");
 					for($i =0; $i<count($files);$i++){
 						$num = $files[$i];
 						if(substr($num,44) !== 'FIRST_gfx' && substr($num, 44) !== "shared"){
-							echo '<p style="float:left;clear:left;display:block;"><a href="?file='.$num.'/tn/*.*'.'">'.substr($num,44).'</a></p>';
+							echo '<p style="float:left;width:32%;padding:10px;display:block;text-indent:0px;text-align:center;"><a href="?file='.$num.'/tn/*.*'.'">'.substr($num,44).'</a></p>';
 						}
 					}
 				}
