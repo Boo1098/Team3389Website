297f243dae1588f4f8251c861266aeca40294d2e
Nathan Spicer-Davis <boo1098@live.com>
2016-02-24 18:34:15 -0500

    margin

--- community.php
+++ community.php
@@ -12,8 +12,8 @@ include("includes/head.php");
 				
 				<img style="width:506px;float:left;padding-right:20px;padding-left:20px;" class="full_mobile" src="resources/community/beach-min.jpg" alt="team">
 								
-				<div class="text">
-					<p style="padding:0px">Our team, the Tec Tigers, works constantly to spread the message of <em>FIRST<sup>&reg;</sup></em>  to our surrounding region. <em>FIRST</em> is all about being able to work together to face problems and accomplish set goals.We as a group also try to get involved with the community and environment through volunteer work.  Some notable activities we have done recently include a beach cleanup, store cleanup, and the setup and cleanup of a 10k run.  In addition to those smaller projects, we have supplied many materials to other FIRST teams in our area.  The Tec Tigers have even gone to the extent of starting two FLL teams on the Eastern Shore of Maryland.</p>
+				<div class="text" style="padding:0px;">
+					<p style="padding:0px;margin:0px;">Our team, the Tec Tigers, works constantly to spread the message of <em>FIRST<sup>&reg;</sup></em>  to our surrounding region. <em>FIRST</em> is all about being able to work together to face problems and accomplish set goals.We as a group also try to get involved with the community and environment through volunteer work.  Some notable activities we have done recently include a beach cleanup, store cleanup, and the setup and cleanup of a 10k run.  In addition to those smaller projects, we have supplied many materials to other FIRST teams in our area.  The Tec Tigers have even gone to the extent of starting two FLL teams on the Eastern Shore of Maryland.</p>
 				</div>
 				
 				<div style="clear:both;margin-bottom:20px;"></div>
