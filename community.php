1b34520567f4fb9aaa31b3c5be92c614017fca59
Madison McLaren <mclarenm01185@students.wcboe.org>
2016-02-24 15:55:32 -0500

    added community page

---
+++ community.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<?php 
+$title = "Community";
+include("includes/head.php");
+?>
+
+	<div class="main-body-text " id="top">
+		<h2 class="body-header   font-effect-3d-float">Community work</h2>
+
+		<div id="about-us-content" style="width:100%">
+			<div style="width:100%" class="content">
+				
+				<img style="width:506px;float:left;padding-right:20px" class="full_mobile" src="resources/community/beach.jpg" alt="team">
+								
+				<div class="text">
+					<p>Our team, the Tec Tigers, works constantly to spread the message of <em>FIRST&reg;</em>  to our surrounding region. <em>FIRST</em> is all about being able to work together to face problems and accomplish set goals.We as a group also try to get involved with the community and environment through volunteer work.  Some notable activities we have done recently include a beach cleanup, store cleanup, and the setup and cleanup of a 10k run.  In addition to those smaller projects, we have supplied many materials to other FIRST teams in our area.  The Tec Tigers have even gone to the extent of starting two FLL teams on the Eastern Shore of Maryland.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<?php include("includes/footer.php"); ?>
+</html>
