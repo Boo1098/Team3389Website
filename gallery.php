dc922e2c3b34f6c54b5e4649582a143f1cc2d2db
Nathan Spicer-Davis <boo1098@live.com>
2016-02-24 20:50:08 -0500

    tese

--- gallery.php
+++ gallery.php
@@ -15,17 +15,26 @@ include("includes/head.php");
 			
 				if(isset($_GET['file'])){
 				$files = glob($_GET['file']);
-					for ($i=0; $i<count($files); $i++)
-					{
+					if(strpos($_GET['file'], '*/*')){}
+						for ($i=0; $i<count($files); $i++)
+						{
+							$num = $files[$i];
+							echo '<a href="'.'http://www.team3389.info/old_site/img2/galleries/2010-Chesapeake_Regional/'.substr($num,75).'"><img src="'.'http://'.substr($num,6).'" alt="'.substr($num,75,-4).'" style="float:left;" /></a>';				
+						}
+					} else {
+						for($i =0; $i<count($files);$i++){
 						$num = $files[$i];
-						echo '<a href="'.'http://www.team3389.info/old_site/img2/galleries/2010-Chesapeake_Regional/'.substr($num,75).'"><img src="'.'http://'.substr($num,6).'" alt="'.substr($num,75,-4).'" style="float:left;" /></a>';				
+						if(substr($num,40) !== 'FIRST_gfx' && substr($num, 40) !== "shared"){
+							echo '<p style="float:left;width:30%;padding:10px;display:block;text-indent:0px;text-align:center;"><a href="?file='.$num.'/tn/*.*'.'">'.substr($num,40).'</a></p>';
+						}
+					}
 					}
 				} else {
 					$files = glob("../../team3389.info/resources/galleries/*");
 					for($i =0; $i<count($files);$i++){
 						$num = $files[$i];
 						if(substr($num,40) !== 'FIRST_gfx' && substr($num, 40) !== "shared"){
-							echo '<p style="float:left;width:32%;padding:10px;display:block;text-indent:0px;text-align:center;"><a href="?file='.$num.'/tn/*.*'.'">'.substr($num,40).'</a></p>';
+							echo '<p style="float:left;width:30%;padding:10px;display:block;text-indent:0px;text-align:center;"><a href="?file='.$num.'/tn/*.*'.'">'.substr($num,40).'</a></p>';
 						}
 					}
 				}
