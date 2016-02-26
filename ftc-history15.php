ceb2b596f90c68d059795f036c608589b9be5248
Nathan Spicer-Davis <boo1098@live.com>
2016-02-26 18:59:23 -0500

    updated history

---
+++ ftc-history15.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<?php 
+$title = "FRC History";
+include("includes/head.php");
+?>
+	<div class="main-body-text " id="top">
+		<h2 class="body-header   font-effect-3d-float">2014-2015</h2>
+		
+		<div id="about-us-content" style="width:100%" align="center">
+			<div align="center" style="width:100%" class="content">
+			
+				<img style="width:506px; float: right;padding-left:20px" class="full_mobile" src="http://www.team3389.info/resources/TeamPic2016.jpg" alt="team"></img>
+
+				<h2 class="body-header">2016 Game Animation - <a href="https://www.youtube.com/watch?v=VqOKzoHJDjA" target="_blank">Recycle Rush</a></h2>
+
+				<div class="text">
+					<p class="center"># Students: 27</p>
+					<p class="center">Regional Attending: TBA</p>
+					
+					<p>This season was the very first year of our <em>FTC</em> team 10382, which branched off of <em>FRC</em> team 3389 in 2015.  It was founded by David Miles. With our block-suck-and-dump robot, WALF, which stands for “We All Love FIRST”, the team placed 3rd at our second competition which qualified us for states.  From the beginning we knew we wanted a bot that could climb.  After much prototyping and brainstorming, it was decided to use treads with “flipper flappers” attached to them, a very <em>“unique”</em> design that ended up working pretty well.  All and all, we placed 12th at regional and ended up in the top 6 percentile of all <em>FTC</em> teams.</p>
+					<div style="clear:both;"></div>
+					<a style="float:left" href="history13.php">&lt;-Previous Year</a><a style="float:right" href="#">Next Year-&gt;</a>
+					<div style="clear:both"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<?php include("includes/footer.php"); ?>
+</html>
---
+++ history15.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<?php 
+$title = "FRC History";
+include("includes/head.php");
+?>
+	<div class="main-body-text " id="top">
+		<h2 class="body-header   font-effect-3d-float">2014-2015</h2>
+		
+		<div id="about-us-content" style="width:100%" align="center">
+			<div align="center" style="width:100%" class="content">
+			
+				<img style="width:506px; float: right;padding-left:20px" class="full_mobile" src="http://www.team3389.info/resources/TeamPic2016.jpg" alt="team"></img>
+
+				<h2 class="body-header">2016 Game Animation - <a href="https://www.youtube.com/watch?v=VqOKzoHJDjA" target="_blank">Recycle Rush</a></h2>
+
+				<div class="text">
+					<p class="center"># Students: 27</p>
+					<p class="center">Regional Attending: TBA</p>
+					
+					<p>Information will be added as we compete in future competitions.</p>
+					<div style="clear:both;"></div>
+					<a style="float:left" href="history13.php">&lt;-Previous Year</a><a style="float:right" href="#">Next Year-&gt;</a>
+					<div style="clear:both"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<?php include("includes/footer.php"); ?>
+</html>
