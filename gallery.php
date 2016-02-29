e1036a9ece1a7da9c91e881b739d415176801914
Eric Carey <careye01465@students.wcboe.org>
2016-02-29 15:50:28 -0500

    target blank

--- gallery.php
+++ gallery.php
@@ -44,7 +44,7 @@ include("includes/head.php");
 							$filename=basename($num);
 							$tn = str_replace($filename, "tn/tn_".$filename, $num);
 							if($filename !== 'tn' && substr($filename, -3) !== "MP4") {
-								echo '<a style="padding:10px;" href="'.$num.'"><img style="height:112px;"src="'.$tn.'" alt="'.$filename.'" /></a>';
+								echo '<a style="padding:10px;" href="'.$num.'" target="_blank" ><img style="height:112px;"src="'.$tn.'" alt="'.$filename.'" /></a>';
 							}
 							if(substr($filename,-3) == "MP4"){
 								echo '<a style="padding:10px;position:relative;bottom:55px;" href="'.$num.'">'.$filename.'</a>';
