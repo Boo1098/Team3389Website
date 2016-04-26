08b9dd255645711f974298ecf1e2b10b6a770d16
Nathan Spicer-Davis <boo1098@live.com>
2016-04-26 16:42:09 -0400

    cleanup

--- Sponsors.php
+++ Sponsors.php
@@ -6,20 +6,6 @@ include("includes/head.php");
 	<div class="main-body-text" height="400" id="top">
 		<h2 class="body-header font-effect-3d-float">2015 Sponsors</h2>
 		<div id="about-us-content" height="200 "align="center">
-			<!--<img src="http://team3389.info/resources/sponsors/new/Nasa_Logo.jpg" class="sponsor-images" alt="nasa" height="200">
-			<img src="http://team3389.info/resources/sponsors/new/Rommel%20s%20Ace%20Logo%20PDF-page-001.jpg" class="sponsor-images" alt="ace" height="200">
-			<img src="http://team3389.info/resources/sponsors/new/Parkside-CTE.png" class="sponsor-images" alt="Parkside CTE" height="200">
-			<img src="http://team3389.info/resources/sponsors/new/TecFoundation.png" class="sponsor-images" alt="TEC hub" height="200">
-			<img src="http://team3389.info/resources/sponsors/new/ChesapeakePediatricsLogo-web-complete.png" class="sponsor-images" alt="Chesapeake Pediatrics" height="200" />
-			<img src="http://team3389.info/resources/sponsors/new/FARMER%20S%20LOGO%20BLUE%20(3)-page-001.jpg" class="sponsor-images" alt="Farmer's bank" height="200" />			<img src="http://team3389.info/resources/sponsors/new/SolidWorks.jpg" class="sponsor-images" alt="SolidWorks" height="200" />
-			<img src="http://team3389.info/resources/sponsors/new/WFF%20Logo%202007.JPG" class="sponsor-images" alt="WFF" height="200" />
-			<img src="http://team3389.info/resources/sponsors/new/WicomicoCountyBoardofEducationLogo.jpg" class="sponsor-images" alt="Wicomico Board of Education" height="200" />
-			<img src="http://team3389.info/resources/sponsors/new/WicomicoPartneershipLogo.png" class="sponsor-images" alt="Wicomico Partnership" height="200" />
-			<img src="http://team3389.info/resources/sponsors/new/autodesk.jpg" class="sponsor-images" alt="AutoDesk" height="200" />
-			<img src="http://team3389.info/resources/sponsors/new/D3.svg" class="sponsor-images" alt="D3 Corp" height="200" />
-			<img src="http://team3389.info/resources/sponsors/new/futuristic-logo-onDark-100high.png" class="sponsor-images" alt="Futuristic" height="200" />
-			<img src="http://team3389.info/resources/sponsors/new/mader.gif" class="sponsor-images" alt="Mader Systems Consulting" height="100" style="background:white" />
-			<img src="resources/sponsors/PeninsulaAllergyandAsthma.png" class="sponsor-images" alt="Peninsula Allergy and Asthma" height="200" />-->
 			<?php
 				$path = "resources/sponsors/new/*";
 				$files = glob($path);
@@ -28,7 +14,7 @@ include("includes/head.php");
 					$filename=basename($num);
 					//$tn = str_replace($filename, "tn/tn_".$filename, $num);
 					if($filename !== 'tn' && substr($filename, -3) !== "MP4") {
-						echo '<a style="padding:10px;" href="'.$num.'" target="_blank" ><img style="height:200px;" class="sponsor-images" src="'.$num.'" alt="'.$filename.'" /></a>';
+						echo '<a style="padding:10px;" href="'.$num.'" target="_blank" ><img style="max-height:200px;" class="sponsor-images" src="'.$num.'" alt="'.$filename.'" /></a>';
 					}
 					/*if(substr($filename,-3) == "MP4"){
 						echo '<a style="padding:10px;position:relative;bottom:55px;" href="'.$num.'">'.$filename.'</a>';
